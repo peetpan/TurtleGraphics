4abfc9b8e988fd4c666f74662e450cd123946bb3
peetpan <peetamber.p@somaiya.edu>
2021-04-03 17:54:24 +0530

    updated file

--- Turtle-Race/main.py
+++ Turtle-Race/main.py
@@ -1,18 +1,30 @@
 from turtle import Turtle, Screen
-
+import random
 
 screen = Screen()
 screen.setup(width=500, height=500)
+bet_color = screen.textinput(title='Make a Bet',
+                             prompt='Which turtle will win the race? (Red, Blue, Green, Yellow, Purple, orange')
+colours = ['red', 'blue', 'green', 'yellow', 'purple', 'orange']
+y_cord = [-70, -40, -10, 20, 50, 80]
+turtle_list = []
+for i in range(0, 6):
+    turtle = Turtle(shape='turtle')
+    turtle.penup()
+    turtle.color(colours[i])
+    turtle.goto(-240, y_cord[i])
+    turtle_list.append(turtle)
 
-turtle_red = Turtle()
-turtle_red.shape('turtle')
-turtle_red.goto(-240, 0)
-turtle_red.color('red')
-
-turtle_blue = Turtle()
-turtle_blue.shape('turtle')
-
-
-
+race_is_on = True
+while race_is_on:
+    for turtle in turtle_list:
+        if turtle.xcor() > 230:
+            race_is_on = False
+            if turtle.pencolor() == bet_color:
+                print(f'You win!! The {turtle.pencolor()} is the winner')
+            else:
+                print(f'You lost!! The {turtle.pencolor()} is the winner')
+            break
+        turtle.forward(random.randint(0, 10))
 
 screen.exitonclick()
